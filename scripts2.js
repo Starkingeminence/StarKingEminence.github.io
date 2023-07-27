53a7e95e3fb39bf47bb07b77d80ee3ef0f6e26c1
Starkingeminence <126306717+Starkingeminence@users.noreply.github.com>
2023-07-27 22:11:05 +0100

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -1,33 +1,15 @@
-// Function to show the popup
-function showPopup() {
-  var popup = document.getElementById("popup");
-  popup.classList.remove("hidden");
-}
-
-// Function to hide the popup
-function cancelPopup() {
-  var popup = document.getElementById("popup");
-  popup.classList.add("hidden");
-}
-
-// Function to continue to the next page
-function continueToNextPage() {
-  window.location.href = "final_page.html";
-}
-
-// Your existing JavaScript code (from the previous code)
-
-// Function to show/hide the video and popup
-function toggleVideoPopup() {
-  const video = document.querySelector('video');
+window.onload = function() {
+  const videoContainer = document.getElementById('videoContainer');
+  const video = document.getElementById('confettiVideo');
   const popup = document.querySelector('.popup');
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-  popup.classList.toggle('show');
-}
 
-// Add event listener to toggle video and popup on click
-document.querySelector('.video-container').addEventListener('click', toggleVideoPopup);
+  video.onended = function() {
+    video.currentTime = 0;
+    video.play();
+  };
+
+  popup.onclick = function() {
+    videoContainer.style.display = 'block';
+    video.play();
+  };
+};
