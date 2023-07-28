bcef92804983fb920fb04ca3cbf8123a9da8861c
Starkingeminence <126306717+Starkingeminence@users.noreply.github.com>
2023-07-28 21:46:07 +0100

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -2,12 +2,17 @@ window.onload = function() {
   const videoContainer = document.getElementById('videoContainer');
   const popup = document.getElementById('popup');
 
+  const videoPath = "confetti-video.mp4"; // Replace "your_video.mp4" with the correct path or filename of your video
+
   function closeVideoPopup() {
     videoContainer.style.display = 'none';
   }
 
   popup.onclick = function() {
+    videoContainer.innerHTML = `<video id="confettiVideo" autoplay loop muted>
+      <source src="${videoPath}" type="video/mp4">
+    </video>`;
     videoContainer.style.display = 'block';
-    setTimeout(closeVideoPopup, 5000); // Adjust the time in milliseconds (5000ms = 5 seconds) for how long the popup stays open
+    setTimeout(closeVideoPopup, 10000); // Adjust the time in milliseconds (5000ms = 5 seconds) for how long the popup stays open
   };
 };
